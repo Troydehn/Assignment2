fdef3175edd82674580a8e3669f21d944d69fd35
Tristan Leao <tlea0003@student.monash.edu>
2021-10-06 20:46:44 +1100

    Added new data files

---
+++ Processing/Stacked_Bar.py
@@ -0,0 +1,1 @@
+df = pandas.read_csv
--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,10 @@
 var vg_1 = "map.json";
-var vg_2 = "s"
+var vg_2 = "DoC.json"
 
 vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+vegaEmbed("#DoC", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
