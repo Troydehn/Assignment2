7d7cd32c0ee459e96820849e9516d29788a19281
Tristan Leao <tlea0003@student.monash.edu>
2021-10-07 19:29:03 +1100

    Cleaned up data to include 2016 and added JSON for heatmap

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,7 @@
 var vg_1 = "map.json";
-var vg_2 = "DoC.json"
+var vg_2 = "DoC.json";
+var vg_3 = "CvP.json";
+var vg_4 = "heatmap.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -7,4 +9,12 @@ vegaEmbed("#map", vg_1).then(function(result) {
 
 vegaEmbed("#DoC", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-   }).catch(console.error);
+}).catch(console.error);
+
+vegaEmbed("#CvP", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#heatmap", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
